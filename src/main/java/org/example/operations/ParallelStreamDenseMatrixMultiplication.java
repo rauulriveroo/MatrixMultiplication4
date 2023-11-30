7f944f23b42184634db2fcc85ee2f8b1ddb1c6df
rauul <rauulriveroo@gmail.com>
2023-11-30 00:22:56 +0000

    added ParallelStreamDenseMatrixMultiplication

--- src/main/java/org/example/operations/ParallelStreamDenseMatrixMultiplication.java
+++ src/main/java/org/example/operations/ParallelStreamDenseMatrixMultiplication.java
@@ -2,6 +2,7 @@ package org.example.operations;
 
 import org.example.matrices.DenseMatrix;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class ParallelStreamDenseMatrixMultiplication implements MatrixMultiplication {
@@ -9,20 +10,25 @@ public class ParallelStreamDenseMatrixMultiplication implements MatrixMultiplica
     private DenseMatrix a;
     private DenseMatrix b;
     private DenseMatrix c;
+    private AtomicInteger rowCounter;
 
     public ParallelStreamDenseMatrixMultiplication(DenseMatrix a, DenseMatrix b) {
         this.a = a;
         this.b = b;
         this.c = new DenseMatrix(new double[a.size()][b.size()]);
+        this.rowCounter = new AtomicInteger(0);
     }
 
     public void multiply() {
         int size = a.size();
 
         IntStream.range(0, size).parallel().forEach(i -> {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    c.getValues()[i][j] += a.getValues()[i][k] * b.getValues()[k][j];
+            int row;
+            while ((row = rowCounter.getAndIncrement()) < size) {
+                for (int j = 0; j < size; j++) {
+                    for (int k = 0; k < size; k++) {
+                        c.getValues()[row][j] += a.getValues()[row][k] * b.getValues()[k][j];
+                    }
                 }
             }
         });
